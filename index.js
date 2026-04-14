a7e4a268a20feba00499feaa927069dbeee105cd
Edvards999 <edvards.kublins@gmail.com>
2026-04-14 10:53:20 +0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const poguSkaits = document.querySelectorAll(".drum").length
-
-for (let i = 0; i < poguSkaits; i++) {
-
-document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-    alert("Man uzklikšķināja!")
-})
-}
